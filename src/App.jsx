90d98f4f6c40dce5a980bfaf0450abb37e3f0ded
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-02-09 08:30:49 +0100

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialNote = [
+    {
+      note: 'Good Morning'
+    },
+    {
+      note: 'Hello Everyone'
+    }
+  ]
+  const [notes, setNote] = useState("")
+  const [initNotes, setInitNote] = useState(initialNote)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInitNote(initNotes.concat({notes: note}));
+    setNote('')
+  }
 
   return (
     <div className="w-[100vw] h-[100vh] bg-[#D9D9D9] p-[10rem]">
-      <h1 className='text-4xl my-[1.5rem] text-[#810D0D] font-[600]'>ADD NOTE</h1>
+      <h1 className='text-4xl my-[1.5rem] text-[#810D0D] font-[700]'>ADD NOTE</h1>
       <form>
-      <input type="text" className='w-[30rem] h-[5rem] border border-black bg-[#D9D9D9] outline-none p-[1.5rem]' />
+        <input type="text" className='w-[30rem] h-[5rem] border border-black bg-[#D9D9D9] outline-none p-[1.5rem]' value={notes} onChange={(e) => setNote(e.target.value)} />
       <br />
       <br />
-      <button className='border border-black w-[7rem] h-[2.5rem] rounded-[45%] text-[#810D0D] mx-[23rem] my-[1rem]'>Add</button>
+      <button type='submit' className='border border-black w-[7rem] h-[2.5rem] rounded-[50%] text-[#810D0D] mx-[23rem] my-[1rem] font-[600]'>Add</button>
 
-      <div></div>
+      <div className='my-[2rem]'>
+        {initNotes.map((initNote, index) =>(
+          <ul key={`${initNote.note} - ${index}`}>
+            <li className='list-disc'>{initNote.note}</li>
+          </ul>
+        ))}
+      </div>
       </form>
     </div>
   )
