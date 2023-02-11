b0ccdb1a44625f8eded85448648291b6fc0ac039
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-02-11 10:03:35 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,35 @@ function App() {
     "Hello everyone"
   ]
   const [notes, setNote] = useState("")
-  const [initNotes, setInitNote] = useState(initialNote)
+  const [CurrentNote, setCurrentNote] = useState(initialNote)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setInitNote(initNotes.concat(notes));
+    setCurrentNote(CurrentNote.concat(notes));
     setNote('')
   }
 
   return (
     <div className="w-[100vw] h-[100vh] bg-[#D9D9D9] p-[10rem]">
       <h1 className='text-4xl my-[1.5rem] text-[#810D0D] font-[700]'>ADD NOTE</h1>
+
       <form onSubmit={handleSubmit}>
+
         <input type="text" className='w-[30rem] h-[5rem] border border-black bg-[#D9D9D9] outline-none p-[1.5rem]' value={notes} onChange={(e) => setNote(e.target.value)} />
+
         <br />
         <br />
+
         <button type='submit' className='border border-black w-[7rem] h-[2.5rem] rounded-[50%] text-[#810D0D] mx-[23rem] my-[1rem] font-[600]'>Add</button>
+        
         <div className='my-[2rem]'>
         <ul>
-        {initNotes.map((aNote, index) =>(
-          
-            <li className='list-disc' key={`${aNote} - ${index}`}>{aNote}</li>
-          
+        {CurrentNote.map((aNote, index) =>(
+          <li className='list-disc' key={`${aNote} - ${index}`}>{aNote}</li>
         ))}
         </ul>
         </div>
+
       </form>
       
     </div>
